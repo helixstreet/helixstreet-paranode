53540cf5ce5eb258e8d04b43d404963479c812ae
helixstreet <53312868+helixstreet@users.noreply.github.com>
2022-10-16 21:49:18 +0200

    helixstreet_paranode

--- node/src/command.rs
+++ node/src/command.rs
@@ -5,7 +5,7 @@ use cumulus_client_cli::generate_genesis_block;
 use cumulus_primitives_core::ParaId;
 use frame_benchmarking_cli::{BenchmarkCmd, SUBSTRATE_REFERENCE_HARDWARE};
 use log::info;
-use helixstreet_paranode_runtime::VERSION::{Block, RuntimeApi};
+use helixstreet_paranode_runtime::{Block, RuntimeApi};
 use sc_cli::{
 	ChainSpec, CliConfiguration, DefaultConfigurationValues, ImportParams, KeystoreParams,
 	NetworkParams, Result, RuntimeVersion, SharedParams, SubstrateCli,
